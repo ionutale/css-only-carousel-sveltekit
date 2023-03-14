66da9360eee0693e0fa5eacf66c20e1292c00c7d
Ion Utale <utale.ion@gmail.com>
2023-03-14 22:12:05 +0100

    using a-tag to scroll and focus the carousel slide

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,51 +1,34 @@
 <script lang="ts">
-  const listOfSlides = [
-    "https://picsum.photos/id/1000/800",
-    "https://picsum.photos/id/1000/800",
-    "https://picsum.photos/id/1001/800",
-    "https://picsum.photos/id/1002/800",
-    "https://picsum.photos/id/1003/800",
-    "https://picsum.photos/id/1004/800",
-    "https://picsum.photos/id/1005/800",
-    "https://picsum.photos/id/1006/800",
-    "https://picsum.photos/id/1006/800",
-  ]
+	const listOfSlides = [
+		'https://picsum.photos/id/1000/800',
+		'https://picsum.photos/id/1000/800',
+		'https://picsum.photos/id/1001/800',
+		'https://picsum.photos/id/1002/800',
+		'https://picsum.photos/id/1003/800',
+		'https://picsum.photos/id/1004/800',
+		'https://picsum.photos/id/1005/800',
+		'https://picsum.photos/id/1006/800',
+		'https://picsum.photos/id/1006/800'
+	];
 
- // scroll the slide number into the middle of the container
- const scrollTo = (e) => {
-    const slideNumber = e.target.dataset.slide
-    const slide = document.getElementById(`slide-${slideNumber}`)
-    const container = document.getElementById('container')
-    const containerWidth = container.offsetWidth
-    const slideWidth = slide.offsetWidth
-    const slideOffset = slide.offsetLeft
-    const scrollPosition = slideOffset - (containerWidth / 2) + (slideWidth / 2)
-    console.log("scrollToPosition", scrollPosition)
-    container.scrollTo({
-      left: scrollPosition,
-      behavior: 'smooth'
-    })
-    // add a hash to the url so the slide can be linked to
-    window.location.hash = `slide-${slideNumber}`
-  }
 </script>
 
 <h1>css only carousel</h1>
 
 <section id="gallery">
-  <article id="container">
-     {#each listOfSlides as slide, i }
-      <div id="slide-{i}" class="slide">
-      {i}
-        <img class="img" src={slide} alt="slide-{i}" />
-      </div>
-    {/each}
-  </article>
-  <article id="segment">
-     {#each listOfSlides as slide, i }
-      <button data-slide={i} class="segmentButton" on:click={scrollTo}>{i}</button>
-    {/each}
-  </article>
+	<article id="container">
+		{#each listOfSlides as slide, i}
+			<div id="slide-{i}" class="slide">
+				{i}
+				<img class="img" src={slide} alt="slide-{i}" />
+			</div>
+		{/each}
+	</article>
+	<article id="segment">
+		{#each listOfSlides as slide, i}
+			<a href="#slide-{i}" class="segmentButton">{i - 1}</a>
+		{/each}
+	</article>
 </section>
 
 <style>
@@ -59,7 +42,8 @@
 	article#container {
 		height: 300px;
 		scroll-snap-type: x mandatory;
-    scroll-behavior: smooth;
+		scroll-behavior: smooth;
+    scroll-snap-align: center;
 		overflow-x: scroll;
 		display: flex;
 		flex-direction: row;
@@ -81,9 +65,9 @@
 		background-color: red;
 	}
 
-  div.slide:target {
-    border: 2px solid blue;
-  }
+	div.slide:target {
+		border: 2px solid blue;
+	}
 
 	div > .img {
 		display: block;
@@ -94,26 +78,35 @@
 	}
 
 	article#container > div:first-child,
-	article#container > div:last-child {
+	article#container > div:last-child,
+	article#segment > a:first-child,
+	article#segment > a:last-child {
 		visibility: hidden;
 	}
 
-  .segmentButton {
-    display: inline-block;
-    width: 3rem;
-    height: 3rem;
-    border-radius: 50%;
-    background-color: #ccc;
-    margin: 0 0.5rem;
-    cursor: pointer;
-  }
+	article#segment {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		gap: 1rem;
+		margin: 1rem 0;
+	}
 
-  .segmentButton:hover {
-    background-color: #aaa;
-  }
+	.segmentButton {
+		display: inline-block;
+		width: 3rem;
+		height: 3rem;
+		border-radius: 50%;
+		background-color: #ccc;
+		margin: 0 0.5rem;
+		cursor: pointer;
+	}
 
-  .segmentButton:active {
-    background-color: #888;
-  }
+	.segmentButton:hover {
+		background-color: #aaa;
+	}
 
+	.segmentButton:active {
+		background-color: #888;
+	}
 </style>
