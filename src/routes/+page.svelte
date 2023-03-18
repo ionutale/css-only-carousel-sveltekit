85f2e5e903ae30fa74dcb9757579a9f2813ea5c8
Ion Utale <utale.ion@gmail.com>
2023-03-18 03:22:44 +0100

    fixed the ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,83 +13,101 @@
 
 </script>
 
-<h1>css only carousel</h1>
-	<p>
-		go ahead and disable the javascript 
-	</p>
-	<code>cmd + shift + p > 'disable javascript'</code>
-	<p>and refresh the page</p>
+<main>
+  <h1>css only carousel</h1>è
+    <p>
+      go ahead and disable the javascript 
+    </p>
+    <code>cmd + shift + p > 'disable javascript'</code>
+    <p>and refresh the page</p>
 
-<section id="gallery">
-	<article id="container">
-		{#each listOfSlides as slide, i}
-			<div id="slide-{i}" class="slide">
-				<img class="img" src={slide} alt="slide-{i}" />
-			</div>
-		{/each}
-	</article>
-	<article id="segment">
-		{#each listOfSlides as slide, i}
-			<a href="#slide-{i}" class="segmentButton">{i}</a>
-		{/each}
-	</article>
-</section>
+  <section id="gallery">
+    <article id="carousel">
+      {#each listOfSlides as slide, i}
+        <div id="slide-{i}" class="slide">
+          <img class="img" src={slide} alt="slide-{i}" />
+        </div>
+      {/each}
+    </article>
+  </section>
+  <article id="segment">
+    {#each listOfSlides as slide, i}
+      <a href="#slide-{i}" class="segmentButton"></a>
+    {/each}
+  </article>
+</main>
 
 <style>
-	section {
-		width: 50vw;
-		height: 300px;
-		max-width: 1440px;
-		margin: 0 auto;
-	}
+  h1 {
+    text-align: center;
+    width: 100%;
+    height: 100%;
+  }
 
-	article#container {
-		height: 300px;
-		scroll-snap-type: x mandatory;
+  main {
+    max-width: 2000px;
+    margin: 0 auto;
+    padding: 1rem;
+  }
+	section {
+		margin: 0 auto;
+    scroll-snap-type: x mandatory;
 		scroll-behavior: smooth;
     scroll-snap-align: center;
 		overflow-x: scroll;
+    width: calc(100vw - 2rem);
+	}
+
+  section::-webkit-scrollbar {
+    display: none;
+  }
+
+	article#carousel {
 		display: flex;
 		flex-direction: row;
 		flex-wrap: nowrap;
 		gap: 1rem;
+    width: fit-content;
 	}
 
 	@media screen and (min-width: 768px) {
-		article#container {
-			gap: 14rem;
-		}
-	}
+    section {
+      width: 500px;
+    }
 
-	article#container div {
-		display: flex;
-		justify-content: center;
-		position: relative;
-		scroll-snap-align: center;
-		background-color: white;
+		article#carousel {
+			gap: 1rem;
+		}
 	}
 
 	div.slide {
 		display: block;
-		background-color: white;
-		width: 300px;
-		height: 300px;
+		background-color: rgb(137, 145, 228);
+		width: 270px;
+		height: 270px;
+    max-width: 300px;
+		position: relative;
+		scroll-snap-align: center;
+    -webkit-box-align: center;
+    aspect-ratio: 1/1;
+    box-sizing: border-box;
+    margin: 0px auto;
+    overflow: hidden;
 	} 
-	
-	/*
-	div.slide:target {
-		border: 2px solid blue;
-	} */
 
 	div.slide:target .img {
-		transform: scale(1.0);
-		transition: all 0.8s 0s;
+		animation: slideActive 0.8s;
+    direction: backwards;
 	}
 
-	div.slide .img {
-		transform: scale(1.2);
-	}
-
+  @keyframes slideActive {
+    0% {
+      transform: scale(1.2);
+    }
+    100% {
+      transform: scale(1.0);
+    }
+  }
 
 	div > .img {
 		display: block;
@@ -97,33 +115,41 @@
 		aspect-ratio: 1/1;
 		height: auto;
 		object-fit: cover;
+    width: 100%;
+    height: 100%;
 	}
 
-	article#container > div:first-child,
-	article#container > div:last-child,
-	article#segment > a:first-child,
-	article#segment > a:last-child {
+	article#carousel > div:first-child,
+	article#carousel > div:last-child {
 		visibility: hidden;
 	}
+
+  article#segment > a:first-child,
+	article#segment > a:last-child {
+    display: none;
+  }
 
 	article#segment {
 		display: flex;
 		justify-content: center;
 		align-items: center;
-		gap: 1rem;
+		gap: 0.1rem;
 		margin: 1rem 0;
 	}
 
 	.segmentButton {
-		display: inline-block;
-		padding: 3rem;
-		border-radius: 50%;
+		padding: 0.25rem 1.5rem;
+		border-radius: 20%;
 		background-color: #ccc;
-		margin: 0 0.5rem;
 		cursor: pointer;
 		text-align: center;
-		vertical-align: center;
 	}
+
+  @media screen and (min-width: 768px) {
+    .segmentButton {
+      padding: 0.25rem 1.5rem;
+    }
+  }
 
 	.segmentButton:hover {
 		background-color: #aaa;
