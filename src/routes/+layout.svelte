b77a2bf147d821a59eae7b78a2878e34145fa89a
Ion Utale <ion.utale@guccidigital.com>
2023-04-16 11:36:27 +0200

    added nav bar and radio button selection

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,38 @@
+<nav>
+	<li><a href="/">home</a></li>
+	<li><a href="/a11y/radio">radio button</a></li>
+	<li><a href="/carousel/simple">simple carousel</a></li>
+</nav>
+
+<slot />
+
+<style>
+	nav {
+		display: flex;
+		list-style: none;
+		margin: 0;
+		padding: 0;
+		position: sticky;
+		top: 0;
+		background: #418db9;
+		color: #fff;
+		height: 4rem;
+	}
+
+	nav li {
+		margin: 0 1rem;
+		color: #fff;
+		display: flex;
+		align-items: center;
+	}
+
+	nav li a {
+		color: #fff;
+		text-decoration: none;
+	}
+
+	nav li a:hover {
+		color: #fff;
+		text-decoration: underline;
+	}
+</style>
---
+++ src/routes/a11y/radio/+page.svelte
@@ -0,0 +1,40 @@
+<article>
+  <label for="radio-1" class="mzp-u-inline">
+    <input type="radio" name="radios" id="radio-1"> Option 1
+  </label>
+  <label for="radio-2" class="mzp-u-inline">
+    <input type="radio" name="radios" id="radio-2"> Option 2
+  </label>
+  <label for="radio-3" class="mzp-u-inline">
+    <input type="radio" name="radios" id="radio-3"> Option 3
+  </label>
+  <label for="radio-4" class="mzp-u-inline">
+    <input type="radio" name="radios" id="radio-4"> Option 4
+  </label>
+  <label for="radio-5" class="mzp-u-inline">
+    <input type="radio" name="radios" id="radio-5"> Option 5
+  </label>
+</article>
+
+<style>
+
+  article {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    gap: 0.5rem;
+  }
+
+  article label {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    gap: 1rem;
+    align-items: center;
+  }
+
+  article label input {
+    margin: 0;
+  }
+
+</style>
